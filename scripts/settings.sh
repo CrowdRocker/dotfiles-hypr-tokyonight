be1afa171b1aba00583fffbe8a775992beb85611
CrowdRocker <berksapexlegends@gmail.com>
2025-08-08 16:51:06 -0500

    update

---
+++ scripts/settings.sh
@@ -0,0 +1,7 @@
+rofi -dmenu -p "Settings" << EOF | sh &
+🌓 Switch Waybar Theme
+⌨️ View Keybinds
+🎮 Toggle GameMode
+💾 ZRAM Status
+🌐 Network Tools
+EOF
---
+++ scripts/welcome.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rofi -dmenu -p "Welcome, Matt!" << EOF | sh &
+🔧 Open Settings
+🎨 Change Theme
+🎮 Launch Games
+📊 System Info
+🎵 Spotify
+⏹️ Power Menu
+EOF
